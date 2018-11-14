ab002fc4bd85543ec63921c0b1ee15ff4c26dca3
rashmihalli <44639046+rashmihalli@users.noreply.github.com>
2018-11-14 23:01:30 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+package com.rashmi.ds;
+import java.util.*;
+public class even {
+
+	public static void main(String[] args) {
+		int n=10,i=1;
+                for(i=1;i<=10;i++)
+                {
+                  if(i%2==0)
+                  System.out.println(i);
+                } 
+
+	}
+
+}
